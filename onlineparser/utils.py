64a290b0d923d3eafba732fda9a4f1de39e57b70
Nicolas Malbran <nmalbran@gmail.com>
2013-08-13 16:28:13 -0400

    Added log parsing, template generation and rendering.

--- onlineparser/utils.py
+++ onlineparser/utils.py
@@ -1,6 +1,6 @@
 
 
 def save_uploaded_file(filename, data):
-    with open(filename, 'wb+') as destination:
+    with open(filename, 'wb') as destination:
         for chunk in data.chunks():
             destination.write(chunk)
--- onlineparser/views.py
+++ onlineparser/views.py
@@ -7,9 +7,11 @@ from django.template import RequestContext
 from django.conf import settings
 
 from forms import LogForm
+from nexuiz_log_parser.nex_parser import NexuizLogParser, get_known_player_nicks
 import utils
 
 class ParserView(View):
+    template_name = 'parsed_log.html'
 
     def get(self, request):
         form = LogForm()
@@ -21,5 +23,15 @@ class ParserView(View):
         if form.is_valid():
             filename = os.path.join(settings.MEDIA_ROOT, request.FILES['log'].name)
             utils.save_uploaded_file(filename, request.FILES['log'])
+            nlp = NexuizLogParser(get_known_player_nicks('nexuiz_log_parser.players.KNOWN_PLAYER_NICKS'))
+            nlp.parse_logs([filename.encode('ascii')])
+            output = nlp.output(output='html')
+
+            temp_template = os.path.join(settings.PROJECT_ROOT, 'onlineparser', 'templates', self.template_name)
+            with open(temp_template, 'w') as dest:
+                dest.write(output.decode('utf-8', 'ignore'))
+
+            return render_to_response(self.template_name, {}, context_instance=RequestContext(request))
+
 
         return render_to_response('parser.html', {'form': form}, context_instance=RequestContext(request))
